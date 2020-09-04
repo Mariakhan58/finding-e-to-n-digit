e69f6314cceea15de756785166910cd67316fa64
Mariakhan58 <59942190+Mariakhan58@users.noreply.github.com>
2020-09-03 22:13:01 -0700

    Add files via upload

---
+++ finding-e.py
@@ -0,0 +1,40 @@
+
+# This program is to calculate e
+# to the user's desired number of digits
+# after decimal
+
+
+import math
+
+print("Note: Please enter a number between 0 and 50.")
+digits = input("How many values you desire after the decimal for e? ")
+value = 1
+
+while value:
+
+    if int(digits) > 0 and int(digits) < 50:
+        print("\nThe desired value of e: %.*f" % (int(digits), math.e))
+        repeat = input("Do you want to run the program again? Press y for yes or n for no. ")
+        if repeat == 'y' or repeat == 'Y':
+            digits = input("How many values you desire after the decimal for PI? ")
+        elif repeat == 'n' or repeat == 'N':
+            print("\nYou chose to exit the program. Good bye.")
+            value = 0
+        else:
+            print("\nInvalid choice. Program terminated.")
+            value = 0
+
+    else:
+        print("\nNumber out of range.")
+        repeat = input("Do you want to run the program again? Press y for yes or n for no. ")
+        if repeat == 'y' or repeat == 'Y':
+            digits = input("How many values you desire after the decimal for PI? ")
+        elif repeat == 'n' or repeat == 'N':
+            print("\nYou chose to exit the program. Good bye.")
+            value = 0
+        else:
+            print("\nInvalid choice. Program terminated.")
+            value = 0
+
+
+
